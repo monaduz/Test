7f8dcd0e6e0fb919c2a610ea8169ec29eae7bbd0
Hubert <canhubert@gmail.com>
2018-06-14 20:57:03 +0200

    commit first by Hubert Java Master

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Teraz commituje ja, Hubert!");
-        System.out.println();
+        System.out.println("kh");
     }
 }
